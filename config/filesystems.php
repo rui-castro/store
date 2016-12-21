493fa1edc300d80fb88fb418779079d6407edf8c
Rui Castro <rui.castro@gmail.com>
2016-12-21 12:10:13 +0000

    Added env variables to config/filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => 'local',
+    'default' => env('FILESYSTEM_DEFAULT', 'local'),
 
     /*
     |--------------------------------------------------------------------------
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'cloud' => 's3',
+    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,10 +56,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => 'your-key',
-            'secret' => 'your-secret',
-            'region' => 'your-region',
-            'bucket' => 'your-bucket',
+            'key' => env('AWS_KEY', 'your-key'),
+            'secret' => env('AWS_SECRET', 'your-secret'),
+            'region' => env('AWS_REGION', 'your-region'),
+            'bucket' => env('AWS_BUCKET', 'your-bucket'),
         ],
 
     ],
