bb9577277ff4dbc6ec6805026edb53ba7a3f04e7
Rui Castro <rui.castro@gmail.com>
2016-12-27 01:35:30 +0000

    Added UsersTableSeeder with guest and admin users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
+use Store\Option;
+use Store\Product;
+use Store\ProductOption;
+use Store\Repositories\ProductRepository;
+use Store\User;
+use Store\Variant;
+use Store\VariantValue;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Guest',
+            'email' => 'guest@galeiras.pt',
+            'password' => Hash::make('galeiras')
+        ]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@galeiras.pt',
+            'password' => Hash::make('g4l31r4s!2017')
+        ]);
+   }
+}
